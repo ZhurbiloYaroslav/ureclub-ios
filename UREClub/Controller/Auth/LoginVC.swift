34a5d7648237e2894c43a36a8623b96d3c14c1a3
YaroslavZhurbilo <4872439@gmail.com>
2017-12-09 12:07:14 +0200

    Added first VCs

---
+++ UREClub/Controller/Auth/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  UREClub
+//
+//  Created by Yaroslav Zhurbilo on 09.12.17.
+//  Copyright © 2017 Yaroslav Zhurbilo. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
---
+++ UREClub/Controller/MenuVC.swift
@@ -0,0 +1,37 @@
+//
+//  MenuVC.swift
+//  UREClub
+//
+//  Created by Yaroslav Zhurbilo on 09.12.17.
+//  Copyright © 2017 Yaroslav Zhurbilo. All rights reserved.
+//
+
+import UIKit
+
+class MenuVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+}
