9074b4f13c71bb980acfa31753f26d6f050d3faa
YaroslavZhurbilo <4872439@gmail.com>
2017-12-14 00:07:42 +0200

    Finished divided Storyboard

---
+++ UREClub/Controller/Menu/MenuManager.swift
@@ -0,0 +1,24 @@
+//
+//  MenuManager.swift
+//  UREClub
+//
+//  Created by Yaroslav Zhurbilo on 09.12.17.
+//  Copyright © 2017 Yaroslav Zhurbilo. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import SWRevealViewController
+
+protocol LMM {
+    
+}
+
+class MenuManager {
+    
+    var menuButton: UIBarButtonItem!
+    
+    init(menuButton button: UIBarButtonItem) {
+        menuButton = button
+    }
+}
